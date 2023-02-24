54d5228edd7051b8fd5c3ae662ea4ff98dae1f5b
Jamon Bailey <infinity.dev@outlook.com>
2023-02-24 18:05:54 -0500

    Create svc.py

---
+++ fluxr/svc.py
@@ -0,0 +1,16 @@
+
+""" FLUX Runtime-Engine Framework Service Provider """
+
+
+#   MODULE IMPORTS
+from fluxr import *
+...
+
+
+#   MODULE PACKAGE
+__package__ = pkg_n()
+
+
+#   MODULE CLASSES
+class ServiceProvider:
+    pass
