a37fe8d189613c60a777ea20138b77d12f3109eb
Jamon Bailey <infinity.dev@outlook.com>
2023-02-24 16:08:45 -0500

    FLUX Runtime-Engine Package Initialization
    Implements package root file skeletons

---
+++ dev.py
@@ -0,0 +1,8 @@
+
+#   FRAMEWORK IMPORT
+...
+
+
+#   DEV
+if __name__ == '__main__':
+    pass
---
+++ fluxr/__init__.py
@@ -0,0 +1,21 @@
+
+""" INFINITY(c) Systems 2023; FLUX Runtime-Engine Framework """
+
+
+#   EXTERNAL IMPORTS
+...
+
+
+#   PACKAGE META
+...
+
+
+#   PACKAGE METHODS
+...
+
+
+#   MODULE IMPORTS
+...
+
+
+pass
