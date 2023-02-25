1d6d13078dbc091a38ab3a9a32b7aeae48406ddd
Jamon Bailey <infinity.dev@outlook.com>
2023-02-24 19:17:08 -0500

    Update exc_types.py

--- fluxr/exc_types.py
+++ fluxr/exc_types.py
@@ -4,6 +4,8 @@
 
 #   MODULE CLASSES
 class ExcFailureError(Exception):
-    def __init__(self):
+    def __init__(self, value):
         self.value: str = 'The runtime exception manager failed'
+        if len(str(value)) > 0:
+            self.value += f': {value}'
         return
