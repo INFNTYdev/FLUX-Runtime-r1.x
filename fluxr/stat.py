db5af8b27686d739c1fc32833e751d2a51b0d661
Jamon Bailey <infinity.dev@outlook.com>
2023-02-25 12:43:30 -0500

    Update stat.py

--- fluxr/stat.py
+++ fluxr/stat.py
@@ -14,4 +14,37 @@ __package__ = pkg_n()
 class FrameworkStatusManager:
     def __init__(self, fw: any):
         """ Framework status module """
+        self.__status: dict = {
+            'exception_ready*': [FrameworkExceptionManager, False],
+            'service_ready*': [ServiceProvider, False],
+            # '#_ready': [None, False],
+            # '#_ready': [None, False],
+            # '#_ready': [None, False],
+            # '#_ready': [None, False],
+            # '#_ready': [None, False],
+            # '#_ready': [None, False],
+            # '#_ready': [None, False],
+            # '#_ready': [None, False],
+            'framework_ready': [RuntimeFramework, False],
+        }
+        return
+
+    def get(self, module: any) -> bool:
+        """ Returns the status of the requested module """
+        return
+
+    def set(self, module: any, status: bool):
+        """ Set the status of a system module """
+        return
+
+    def core_systems_active(self) -> bool:
+        """ Determines if required modules are active """
+        return
+
+    def all_systems_active(self) -> bool:
+        """ Determines if all modules are actvie """
+        return
+
+    def __status_exist(self, module: any) -> bool:
+        """ Determines if the provided modules status exist """
         return
