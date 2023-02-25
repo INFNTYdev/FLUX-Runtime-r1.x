057e45539773d3b7afece956bc3ac83c3e0bb3df
Jamon Bailey <infinity.dev@outlook.com>
2023-02-24 19:06:18 -0500

    Update __init__.py

--- fluxr/__init__.py
+++ fluxr/__init__.py
@@ -33,6 +33,7 @@ def pkg_v() -> str:
 #   MODULE IMPORTS
 from .const import *
 from .ext_cde import *
+from .exc_types import *
 from .exc import FrameworkExceptionManager
 from .svc import ServiceProvider
 from .rfw import RuntimeFramework
