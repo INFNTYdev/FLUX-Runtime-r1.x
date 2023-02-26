830b4fca112651ed24e4a7a571a4bf4924bfc224
Jamon Bailey <infinity.dev@outlook.com>
2023-02-25 20:51:57 -0500

    Update cexc.py

--- fluxr/cexc.py
+++ fluxr/cexc.py
@@ -4,12 +4,10 @@
 
 #   MODULE CLASSES
 class ExcFailureError(Exception):
-
-    notice: str = "\n\n\t[ FATAL RUNTIME ERROR ] : ExcModFailure"
-
     def __init__(self, value=None):
         """ Framework exception manager failure """
         self.value: str = 'The runtime exception manager failed'
+        self.notice: str = "\n\n\t[ FATAL RUNTIME ERROR ] : ExcModFailure"
         if value is not None:
             self.value += f': {value}'
         return
