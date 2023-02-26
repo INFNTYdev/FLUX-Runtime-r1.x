594e8351160acb818a1e8a9233426bdbd5f080bd
Jamon Bailey <infinity.dev@outlook.com>
2023-02-25 20:45:19 -0500

    Update dev.py

--- dev.py
+++ dev.py
@@ -6,4 +6,4 @@ from fluxr import *
 #   DEV
 if __name__ == '__main__':
     print(pkg_n(), pkg_v())
-    sys: RuntimeFramework = RuntimeFramework()
+    sys: RuntimeFramework = RuntimeFramework(dev=True)
