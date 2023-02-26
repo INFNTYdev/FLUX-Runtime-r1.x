8194135d59a5bb71b82e112d9ddb7f467c2ba351
Jamon Bailey <infinity.dev@outlook.com>
2023-02-25 23:36:17 -0500

    Update svc.py

--- fluxr/svc.py
+++ fluxr/svc.py
@@ -17,10 +17,17 @@ class ServiceProvider:
     __whitelist: dict = {}
     __admin: list = []
 
+    HIGH: int = 3
+    MED: int = 2
+    LOW: int = 1
+    ANY: int = 0
+
     def __init__(self, fw: any, **kwargs):
         """ Framework service provider """
         self.__FW = fw_obj(fw)
-        fw.console_out("Initializing service provider...")
+        self.__S: dict = fw.base_services()
+
+        self.__out("Initializing service provider...")
         self.__serve: dict = {}
         return
 
@@ -32,6 +39,23 @@ class ServiceProvider:
         """ Returns list of all current services """
         return
 
+    def new_service(self, call: str, cls: type, func: any, clearance: str):
+        """ Add a function to the service provider """
+        return
+
     def serve(self, requestor: any) -> dict:
         """ Returns appropriate services to requestor """
+        return
+
+    def whitelist_class(self, requestor: type, cls: any, clearance: str):
+        """ Add a class to the providers whitelist """
+        return
+
+    def authorize_class(self, requestor: type, cls: any):
+        """ Add a class to the providers administration """
+        return
+
+    def __out(self, text: str, **kwargs):
+        """ Send text to the console """
+        self.__S['console'](text, **kwargs)
         return
