6e238b8bfdb311759d1adc8a6f25bfbd9a86f279
Jamon Bailey <infinity.dev@outlook.com>
2023-02-25 23:21:47 -0500

    Update svc.py

--- fluxr/svc.py
+++ fluxr/svc.py
@@ -13,6 +13,21 @@ __package__ = pkg_n()
 
 #   MODULE CLASSES
 class ServiceProvider:
-    def __init__(self):
+    def __init__(self, fw: any, **kwargs):
         """ Framework service provider """
+        self.__FW = fw_obj(fw)
+        fw.console_out("Initializing service provider...")
+        self.__serve: dict = {}
+        return
+
+    def __init_attr(self, **kwargs):
+        """ Initialize provider parameters """
+        return
+
+    def services(self) -> list:
+        """ Returns list of all current services """
+        return
+
+    def serve(self, requestor: any) -> dict:
+        """ Returns appropriate services to requestor """
         return
