ec71f9f9717d2daebec2521a3c60fd7298944926
Jamon Bailey <infinity.dev@outlook.com>
2023-02-25 20:06:42 -0500

    Update rfw.py

--- fluxr/rfw.py
+++ fluxr/rfw.py
@@ -31,14 +31,34 @@ class RuntimeFramework:
         self.__asset_chain: dict = {}
         self.__META_REF = kwargs.get('meta')
         self.__APPLICATION = kwargs.get('application')
-
         self.__status: FrameworkStatusManager = FrameworkStatusManager()
+
+        ...
         return
 
     def system_exit(self, **kwargs):
         """ Shutdown runtime-engine """
         return
 
+    # STATUS METHODS
+    def __set_module_status(self, module: any, status: bool):
+        """ Set the status of a system module """
+        self.__status.set(module, status)
+        return
+
+    def __get_module_status(self, module: any) -> bool:
+        """ Get the current status of a system module """
+        return self.__status.get(module)
+
+    def __core_active(self) -> bool:
+        """ Determines if required modules are active """
+        return self.__status.core_systems_active()
+
+    def __all_active(self) -> bool:
+        """ Determines if all modules are actvie """
+        return self.__status.all_systems_active()
+
+    # BASEMENT METHODS
     def __runnable(self) -> bool:
         """ Determines if system conditions are appropriate """
         return
