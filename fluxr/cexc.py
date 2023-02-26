9ffafaf1c7644c7b88695d2d7c8e00e57eb807a3
Jamon Bailey <infinity.dev@outlook.com>
2023-02-25 23:08:08 -0500

    Update

--- fluxr/cexc.py
+++ fluxr/cexc.py
@@ -7,9 +7,9 @@ class ExcFailureError(Exception):
     def __init__(self, value=None):
         """ Framework exception manager failure """
         self.value: str = 'The runtime exception manager failed'
-        self.notice: str = f"\n\n\t[ FATAL RUNTIME ERROR ] : {self.value}"
         if value is not None:
             self.value += f': {value}'
+        self.notice: str = f"\n\n\t[ FATAL RUNTIME ERROR ] : {self.value}"
         return
 
 
@@ -17,7 +17,7 @@ class SvcFailureError(Exception):
     def __init__(self, value=None):
         """ Framework service provider failure """
         self.value: str = 'The runtime service provider failed'
-        self.notice: str = f"\n\n\t[ FATAL RUNTIME ERROR ] : {self.value}"
         if value is not None:
             self.value += f': {value}'
+        self.notice: str = f"\n\n\t[ FATAL RUNTIME ERROR ] : {self.value}"
         return
