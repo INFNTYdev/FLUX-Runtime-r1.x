283161f333bafa296e123173f19f8da348fe8276
Jamon Bailey <infinity.dev@outlook.com>
2023-02-26 23:01:12 -0500

    Update

--- fluxr/svc.py
+++ fluxr/svc.py
@@ -17,10 +17,10 @@ class ServiceProvider:
     __whitelist: dict = {}
     __admin: list = []
 
-    HIGH: int = 3
-    MED: int = 2
-    LOW: int = 1
-    ANY: int = 0
+    __HIGH: int = 3
+    __MED: int = 2
+    __LOW: int = 1
+    __ANY: int = 0
 
     def __init__(self, fw: any, **kwargs):
         """ Framework service provider """
@@ -35,7 +35,10 @@ class ServiceProvider:
     def __eval_param(self, **kwargs):
         """ Evaluate provider parameters """
         self.authorize_class(requestor=self, cls=self.__FW)
-        self.whitelist_class(requestor=self, cls=self.__FW, clearance=self.HIGH)
+        self.whitelist_class(requestor=self, cls=self.__FW, clearance=self.__HIGH)
+        if ('whitelist' in kwargs) and (kwargs.get('whitelist') is list):
+            for li in kwargs.get('whitelist'):
+                self.whitelist_class(self.__FW, li[0], li[1])
         return
 
     def services(self) -> list:
@@ -53,26 +56,44 @@ class ServiceProvider:
     def whitelist_class(self, requestor: any, cls: any, clearance: str):
         """ Add a class to the providers whitelist """
         if self.__authorized(requestor):
-            pass
+            self.__whitelist[self.p_obj(cls)] = self.__value_to_const(clearance)
         return
 
     def authorize_class(self, requestor: any, cls: any):
         """ Add a class to the providers administration """
-        print(
-            f'\n'
-            f'< REQUESTOR INFORMATION >'
-            f'\nOBJ : {requestor}'
-            f'\nTYPE: {str(type(requestor))}'
-            f'\n{"="*(len(str(requestor))+7)}'
-            f'\n< AUTHORIZEE INFORMATION >'
-            f'\nOBJ : {cls}'
-            f'\nTYPE: {str(type(cls))}'
-            f'\n'
-        )
+        if self.__authorized(requestor):
+            self.__admin.append(self.p_obj(cls))
+            self.__whitelist[self.p_obj(cls)] = self.__HIGH
+        return
+
+    @staticmethod
+    def p_obj(obj: any) -> type:
+        """ Returns the class of a request object """
+        if 'object' in str(obj):
+            return obj.__class__
+        elif inspect.isclass(obj):
+            return obj
         return
 
     def __authorized(self, requestor: any) -> bool:
         """ Determines if requestor has administrative rights """
+        return bool((self.p_obj(requestor) in self.__admin)
+                    or (requestor == self))
+
+    def __value_to_const(self, value: any) -> int:
+        """ Returns class constant value from string or integer """
+        if type(value) is str:
+            if value.lower() == 'h' or value.lower() == 'high':
+                return self.__HIGH
+            elif value.lower() == 'm' or value.lower() == 'medium':
+                return self.__MED
+            elif value.lower() == 'l' or value.lower() == 'low':
+                return self.__LOW
+            elif value.lower() == 'a' or value.lower() == 'any':
+                return self.__ANY
+        elif type(value) is int:
+            if value <= 3:
+                return value
         return
 
     def __out(self, text: str, **kwargs):
