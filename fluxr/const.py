0510854c100fac0f27474bbb55dfa2c83c00cc05
Jamon Bailey <infinity.dev@outlook.com>
2023-02-27 16:15:20 -0500

    Update const.py

--- fluxr/const.py
+++ fluxr/const.py
@@ -6,4 +6,8 @@ RFW_FAIL_NOTICE: str = str(
     f'\n\tUnable to start due to resource failure, press enter to exit...'
     f'\n'
 )
+
+MISSING_APP_ARGS: str = str(
+    "Applications __init__ method missing required 'fw' and 'svc_c' parameters"
+)
 ...
