90e22b4d49d614892954b5048747c467eeb07816
Jamon Bailey <infinity.dev@outlook.com>
2023-02-27 16:08:07 -0500

    Update

--- fluxr/const.py
+++ fluxr/const.py
@@ -2,7 +2,7 @@
 
 #   CONSTANTS
 RFW_FAIL_NOTICE: str = str(
-    f'\n\t[ Runtime Framework Failed ]'
+    f'\n\t[ RUNTIME FRAMEWORK FAILED ]'
     f'\n\tUnable to start due to resource failure, press enter to exit...'
     f'\n'
 )
