0f5ce74704ae06f82c283f011b30d12c4b8ab7d6
Jamon Bailey <infinity.dev@outlook.com>
2023-02-27 13:50:50 -0500

    Update sys_thread.py

--- fluxr/sys_thread.py
+++ fluxr/sys_thread.py
@@ -20,4 +20,11 @@ class SystemThread:
 class SystemThreadManager:
     def __init__(self, fw: any, svc_c: any):
         """ Framework thread manager """
+        self.__FW = fw_obj(fw)
+        self.__S = fw.service_call()
+        return
+
+    def __out(self, text: str, **kwargs):
+        """ Send text to the console """
+        self.__S(self)['console'](text, **kwargs)
         return
