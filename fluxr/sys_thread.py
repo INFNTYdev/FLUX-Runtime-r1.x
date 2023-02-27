9e5986b0f3fbcb69f77a452c3d8d57a4232a49ed
Jamon Bailey <infinity.dev@outlook.com>
2023-02-27 01:14:04 -0500

    Create sys_thread.py

---
+++ fluxr/sys_thread.py
@@ -0,0 +1,23 @@
+
+""" FLUX Runtime-Engine Framework Thread Manager """
+
+
+#   MODULE IMPORTS
+from fluxr import *
+
+
+#   MODULE PACKAGE
+__package__ = pkg_n()
+
+
+#   MODULE CLASSES
+class SystemThread:
+    def __init__(self, handle: str, thread: Thread, **kwargs):
+        """ Framework thread """
+        return
+
+
+class SystemThreadManager:
+    def __init__(self, fw: any, svc_c: any):
+        """ Framework thread manager """
+        return
