7e451f57a4b4c472c2527d4ff0a8f5090696843e
Jamon Bailey <infinity.dev@outlook.com>
2023-02-27 14:05:10 -0500

    Update

--- fluxr/sys_thread.py
+++ fluxr/sys_thread.py
@@ -21,7 +21,7 @@ class SystemThreadManager:
     def __init__(self, fw: any, svc_c: any):
         """ Framework thread manager """
         self.__FW = fw_obj(fw)
-        self.__S = fw.service_call()
+        self.__S = fw.service_call(self)
 
         self.__out("TEST")
         return
