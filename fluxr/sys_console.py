ef9eab9b298fb083f8a6237a727ac4a8b22f36c4
Jamon Bailey <infinity.dev@outlook.com>
2023-02-27 23:18:32 -0500

    Create sys_console.py

---
+++ fluxr/sys_console.py
@@ -0,0 +1,15 @@
+
+""" FLUX Runtime-Engine Framework Console Manager """
+
+
+#   MODULE IMPORTS
+from fluxr import *
+
+
+#   MODULE PACKAGE
+__package__ = pkg_n()
+
+
+#   MODULE CLASSES
+class SystemConsoleManager:
+    pass
