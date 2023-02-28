da7622d8cdfd64f12d3136a6b179624d4c353c3b
Jamon Bailey <infinity.dev@outlook.com>
2023-02-27 21:49:50 -0500

    Create sys_rt.py

---
+++ fluxr/sys_rt.py
@@ -0,0 +1,15 @@
+
+""" FLUX Runtime-Engine Framework Runtime Clock """
+
+
+#   MODULE IMPORTS
+from fluxr import *
+
+
+#   MODULE PACKAGE
+__package__ = pkg_n()
+
+
+#   MODULE CLASSES
+class SystemRuntimeClock:
+    pass
