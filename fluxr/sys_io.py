65d13b3cd3e02da111935d8b03d59c28c095602d
Jamon Bailey <infinity.dev@outlook.com>
2023-02-28 17:47:42 -0500

    Create sys_io.py

---
+++ fluxr/sys_io.py
@@ -0,0 +1,19 @@
+
+""" FLUX Runtime-Engine Framework File I/O Manager """
+
+
+#   MODULE IMPORTS
+from fluxr import *
+
+
+#   MODULE PACKAGE
+__package__ = pkg_n()
+
+
+#   MODULE CLASSES
+class SystemFileIOManager:
+    def __init__(self, fw: any, svc_c: any):
+        """ Framework file I/O manager """
+        self.__FW = fw_obj(fw)
+        self.__S = svc_c
+        return
