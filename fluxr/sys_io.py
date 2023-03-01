d2ae5bd9bc51cd60cf9b65bb1227b552613a0e08
Jamon Bailey <infinity.dev@outlook.com>
2023-02-28 19:31:04 -0500

    Update sys_io.py

--- fluxr/sys_io.py
+++ fluxr/sys_io.py
@@ -28,3 +28,76 @@ class SystemFileIOManager:
         """ Stop framework file manager """
         self.RUN = False
         return
+
+    def files(self) -> list:
+        """ Returns the list of file names in the file host """
+        return
+
+    # FRAMEWORK SERVICE BOILER PLATE - lvl4
+    def __inject_services(self):
+        """ Add class functions to service provider """
+        return
+
+    def __out(self, text: str, **kwargs):
+        """ Send text to the console """
+        self.__S(self)['console'](text, **kwargs)
+        return
+
+    def __status(self, status: bool):
+        """ Update the modules status """
+        self.__S(self)['setstat'](self, status)
+        return
+
+    def __date(self) -> str:
+        """ Returns the current date """
+        return self.__S(self)['date']()
+
+    def __time(self) -> str:
+        """ Returns the current time """
+        return self.__S(self)['time']()
+
+    def __runtime(self) -> str:
+        """ Returns the current runtime """
+        return self.__S(self)['rt']()
+
+    def __fw_stable(self) -> bool:
+        """ Determines if required system modules are active """
+        return self.__S(self)['corestat']()
+
+    def __fw_active(self) -> bool:
+        """ Returns frameworks run status """
+        return self.__S(self)['runstat']()
+
+    def __new_service(self, call: str, cls: any, func: any, **kwargs):
+        """ Add a function reference to the service provider """
+        self.__S(self)['nsvc'](call, cls, func, **kwargs)
+        return
+
+    def __pause_console(self):
+        """ Pause the console queue output """
+        self.__S(self)['pauseConsole']()
+        return
+
+    def __resume_console(self):
+        """ Resume the console queue output """
+        self.__S(self)['pauseConsole']()
+        return
+
+    def __threads(self) -> list:
+        """ Returns a list of stored thread handles """
+        return self.__S(self)['threads']()
+
+    def __new_thread(self, handle: str, thread: Thread, **kwargs):
+        """ Establish new thread in thread host """
+        self.__S(self)['nthread'](handle, thread, **kwargs)
+        return
+
+    def __delete_thread(self, handle: str):
+        """ Delete requested thread """
+        self.__S(self)['dthread'](handle)
+        return
+
+    def __exc(self, cls: any, exc_o: any, exc_info: tuple, **kwargs):
+        """ Handle system raised exceptions """
+        self.__S(self)['exc'](cls, exc_o, exc_info, **kwargs)
+        return
