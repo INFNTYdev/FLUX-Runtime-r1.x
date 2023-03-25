082cd6fe37218b1cd78cbf89ca273b470c25c0a2
Jamon Bailey <infinity.dev@outlook.com>
2023-03-24 20:25:54 -0400

    Implements FLUX Tkinter Application Framework

---
+++ fluxr/ftkinter/__init__.py
@@ -0,0 +1,29 @@
+
+""" INFINITY(c) Systems 2023; FLUX Tkinter Application Framework """
+
+
+#   EXTERNAL IMPORTS
+from fluxr import *
+
+
+#   PACKAGE META
+__PACKAGE_NAME: str = str(__file__).split('\\')[-2]
+__PACKAGE_VERSION: str = '1.0.0.0'
+
+
+#   PACKAGE METHODS
+def tkpkg_n() -> str:
+    """ Returns package name """
+    return __PACKAGE_NAME
+
+
+def tkpkg_v() -> str:
+    """ Returns package version """
+    return __PACKAGE_VERSION
+
+
+#   MODULE IMPORTS
+...
+
+
+pass
