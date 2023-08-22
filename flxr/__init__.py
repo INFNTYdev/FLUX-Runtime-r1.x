1858e391d04ace46d74df708fbbea0cca97007de
Jamon Bailey <jamon.infinity@gmail.com>
2023-08-22 15:53:21 -0400

    Create flxr Package
    Framework python package implementation

---
+++ flxr/__init__.py
@@ -0,0 +1,21 @@
+
+"""
+INFINITY Systems, LLC. 2023 -
+FLUX Runtime Framework
+"""
+
+
+#   THIRD-PARTY IMPORTS
+pass
+
+
+#   BUILT-IN IMPORTS
+pass
+
+
+#   EXTERNAL IMPORTS
+pass
+
+
+#   LOCAL IMPORTS
+pass
